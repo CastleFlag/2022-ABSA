e19ff2bc6d33e6b4a6032869d17b21ff2f677b39
CastleFlag <sgj.googl@gmail.com>
2022-10-20 00:02:39 +0900

    type : feat add train+test .sh file

--- src/train.sh
+++ src/train.sh
@@ -1,10 +1,7 @@
-# MODELS=(bert-base-multilingual-uncased koelectra-base-v3-discriminator xlm-roberta-large)
-# HIDDEN_SIZE=(768 768 1024)
-MODELS=(bert-base-multilingual-uncased)
-HIDDEN_SIZE=(768)
+MODELS=(bert-base-multilingual-uncased koelectra-base-v3-discriminator xlm-roberta-large)
+HIDDEN_SIZE=(768 768 1024)
 
-# for ITER in 0 1 2;
-for ITER in 0;
+for ITER in 0 1 2;
 do
 ./train_M4m7.sh ${MODELS[$ITER]} ${HIDDEN_SIZE[$ITER]}
 done
